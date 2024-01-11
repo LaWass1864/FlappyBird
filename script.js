dabb7eae9818c3aae6e253e388b630d5a6faee44
lawass1864 <amwass4@hotmail.com>
2024-01-11 23:05:51 +0100

    function pipes ok

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ img.src = "./media/flappy-bird-set.png";
 
 // General settings
 let gamePlaying = false;
-const gravity = .5;
+const gravity = 0.5;
 const speed = 6.2;
 const size = [51, 36];
 const jump = -11.5;
@@ -29,11 +29,11 @@ const setup = () => {
     currentScore = 0;
     flight = jump;
       // set initial flyHeight (middle of screen - size of the bird)
-  flyHeight = (canvas.height / 2) - (size[1] / 2);
+      flyHeight = (canvas.height / 2) - (size[1] / 2);
 
-  pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
-  console.log(pipes);
-  }
+      pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
+   
+    }
 
 // Fonction qui va nous rendre l'animation
 const render = () => {
@@ -41,34 +41,18 @@ const render = () => {
   // CanvasRenderingContext2D.drawImage() https://developer.mozilla.org/fr/docs/Web/API/CanvasRenderingContext2D/drawImage
 
   // Background
-  ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -(index * (speed / 2)) % canvas.width, 0, canvas.width, canvas.height);
-  // Le modulo va tourner en rond et va faire battre els ailes de l'oiseau.  Pcq le modulo va faire tourner les 3 oiseaux
-  if (gamePlaying) {
-    ctx.drawImage(img,432,Math.floor((index % 9) / 3) * size[1], ...size, cTenth,  flyHeight, ...size);
-    flight += gravity;
-    flyHeight = Math.min(flyHeight + flight, canvas.height -size[1]);
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
 
-  } else {
-  ctx.drawImage(img,432,Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2),flyHeight, ...size);
-  flyHeight = (canvas.height / 2) - (size[1] / 2);
+   // Pipe Display
 
-  // Ecrire dans le canvas
-  ctx.fillText(`Meilleur score : ${bestScore}`, 55, 245);
-  ctx.fillText('Cliquez pour jouer', 48, 535)
-  // Reglage css
-  ctx.font = "bold 30px courrier";
-  }
-
-  // Pipe Display
-
-  if (gamePlaying) {
+   if (gamePlaying) {
     pipes.map(pipe => {
         pipe[0] -= speed;
 // on va chercher les poteaux dans le set image
-        ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe [0], 0, pipeWidth, pipe[1]);
-
-        // Bottom pipe
-        ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height -pipe[1] + pipeGap);
+         // top pipe
+      ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+      // bottom pipe
+      ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
 
         if (pipe[0] <= -pipeWidth) {
             currentScore++;
@@ -80,11 +64,28 @@ const render = () => {
         }
     })
   }
+  // Le modulo va tourner en rond et va faire battre les ailes de l'oiseau.  Pcq le modulo va faire tourner les 3 oiseaux
+  if (gamePlaying) {
+    ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size);
+    flight += gravity;
+    flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+  } else {
+    ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
+    flyHeight = (canvas.height / 2) - (size[1] / 2);
+      // text accueil
+    ctx.fillText(`Meilleur score : ${bestScore}`, 55, 245);
+    ctx.fillText('Cliquez pour jouer', 48, 535);
+    ctx.font = "bold 30px courier";
+  }
+ 
 
   window.requestAnimationFrame(render);
 };
 
+// launch setup
 setup();
 img.onload = render;
-document.addEventListener('click', () => gamePlaying = true)
+
+// start game
+document.addEventListener('click', () => gamePlaying = true);
 window.onclick = () => flight = jump;
