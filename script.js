c3162af58c49cd0f602c50f2f38da043fdceeb65
lawass1864 <amwass4@hotmail.com>
2024-01-13 11:20:37 +0100

    floppy perroquet ok

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ img.src = "./media/flappy-bird-set.png";
 // Reglages generaux
 
 let gamePlaying = false;
-const gravity =.5;
+const gravity = 0.5;
 // Vitesse des poteaux lorsqu'ils arrivent
 const speed = 6.2;
 //  Taille de l'oiseau
@@ -21,7 +21,9 @@ const cTenth = canvas.width / 10;
 // Pipe settings
 const pipeWidth = 78;
 const pipeGap = 270;
-const pipeLoc = () => (Math.random() * ((canvas.height - (pipeGap +pipeWidth)) - pipeWidth)) + pipeWidth;
+const pipeLoc = () =>
+  Math.random() * (canvas.height - (pipeGap + pipeWidth) - pipeWidth) +
+  pipeWidth;
 
 // Création de l'effet paralax
 let index = 0,
@@ -31,16 +33,18 @@ let index = 0,
   flight,
   flyHeight;
 
-  const setup = () => {
-    currentScore = 0;
-    flight = jump;
-    flyHeight = (canvas.height / 2) - (size[1] / 2);
-    
-// Il faut créer un array de 3 poteaux. Car il doit y avoir tjr 3 poteaux dans l'ecran.
+const setup = () => {
+  currentScore = 0;
+  flight = jump;
+  flyHeight = canvas.height / 2 - size[1] / 2;
 
-    pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
-    console.log(pipes);
-  }
+  // Il faut créer un array de 3 poteaux. Car il doit y avoir tjr 3 poteaux dans l'ecran.
+
+  pipes = Array(3)
+    .fill()
+    .map((a, i) => [canvas.width + i * (pipeGap + pipeWidth), pipeLoc()]);
+  console.log(pipes);
+};
 // Fonction qui va gerer le rendu
 const render = () => {
   index++;
@@ -73,13 +77,17 @@ const render = () => {
 
   if (gamePlaying) {
     ctx.drawImage(
-        img,
-        432,
-        Math.floor((index % 9) / 3) * size[1],
-        ...size, cTenth, flyHeight, ...size);
-        // Eviter que l'oiseau monte trop haut
-        flight += gravity;
-        flyHeight = Math.min(flyHeight + flight, canvas.height -size[1]);
+      img,
+      432,
+      Math.floor((index % 9) / 3) * size[1],
+      ...size,
+      cTenth,
+      flyHeight,
+      ...size
+    );
+    // Eviter que l'oiseau monte trop haut
+    flight += gravity;
+    flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
   } else {
     // Dessin de l'oiseau
     //  The image object (as returned by document.getElementById ) The X coordinate, The Y coordinate,The width to draw the image,The height to draw the image
@@ -99,26 +107,63 @@ const render = () => {
     ctx.fillText(`Cliquez pour jouer`, 48, 535);
     ctx.font = "bold 30px courier";
   }
-// Pipe display
-if (gamePlaying) {
-    pipes.map(pipe => {
-        pipe[0] -= speed;
-        // top pipe
-        ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
-        // bottom pipe
-        ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
+  // Pipe display
+  if (gamePlaying) {
+    pipes.map((pipe) => {
+      pipe[0] -= speed;
+      // top pipe
+      ctx.drawImage(
+        img,
+        432,
+        588 - pipe[1],
+        pipeWidth,
+        pipe[1],
+        pipe[0],
+        0,
+        pipeWidth,
+        pipe[1]
+      );
+      // bottom pipe
+      ctx.drawImage(
+        img,
+        432 + pipeWidth,
+        108,
+        pipeWidth,
+        canvas.height - pipe[1] + pipeGap,
+        pipe[0],
+        pipe[1] + pipeGap,
+        pipeWidth,
+        canvas.height - pipe[1] + pipeGap
+      );
 
-        if (pipe[0] <= -pipeWidth) {
-            currentScore++;
-            bestScore = Math.max(bestScore, currentScore);
+      if (pipe[0] <= -pipeWidth) {
+        currentScore++;
+        bestScore = Math.max(bestScore, currentScore);
 
+        //  supprimer un poteau + en crée un nouveau
+        pipes = [
+          ...pipes.slice(1),
+          [pipes[pipes.length - 1][0] + pipeGap + pipeWidth, pipeLoc()],
+        ];
+      }
+      // if hit the pipe, end
+      if (
+        [
+          pipe[0] <= cTenth + size[0],
+          pipe[0] + pipeWidth <= cTenth,
+          pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1],
+        ].every((elem) => elem)
+      ) {
+        gamePlaying = false;
+        setup();
+      }
+    });
+  }
 
-            //  remove pipe + create new one 
-            pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc()]]
-            console.log(pipes);
-        }
-    })
-}
+  document.getElementById("bestScore").innerHTML = `Meilleur : ${bestScore}`;
+  document.getElementById(
+    "currentScore"
+  ).innerHTML = `Actuel : ${currentScore}`;
   window.requestAnimationFrame(render);
 };
 
@@ -126,6 +171,6 @@ if (gamePlaying) {
 img.onload = render;
 
 // Clique sur l'oiseau
-setup()
+setup();
 document.addEventListener("click", () => (gamePlaying = true));
-window.onclick = () => flight = jump;
+window.onclick = () => (flight = jump);
